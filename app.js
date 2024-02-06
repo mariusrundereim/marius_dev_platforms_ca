26843c3481ebad58ce5d78d21a2cfcb9efa1dc7f
Marius Rundereim <marius.rundereim@gmail.com>
2024-02-06 21:34:13 +0100

    starting with events

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
 import express from "express";
-import { connectDatabase, closeDatabase } from "./database/database.js";
 import "dotenv/config.js";
 import venuesRoutes from "./routes/venuesRoutes.js";
+import eventsRoutes from "./routes/eventsRoutes.js";
 
 const port = process.env.PORT;
 const app = express();
 app.use(express.json());
 
-//check if it works
-/*
-async function run() {
-  try {
-    const db = await connectDatabase("admin");
-    await db.command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    await closeDatabase();
-  }
-}
-run().catch(console.dir);
-*/
 app.use("/", venuesRoutes);
+app.use("/", eventsRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port http://localhost:${port}`);
---
+++ controllers/eventsController.js
@@ -0,0 +1,11 @@
+import { connectDatabase, closeDatabase } from "../database/database.js";
+import { ObjectId } from "mongodb";
+
+// Get a single event by ID
+// GET
+
+// Get all ticket types for a specitic event
+// GET
+
+// Get all feedback for a specific event
+// GET
