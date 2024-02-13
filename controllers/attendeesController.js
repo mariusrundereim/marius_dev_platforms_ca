185148435cdbd1473de98343988f86ffee97b92a
Marius Rundereim <marius.rundereim@gmail.com>
2024-02-13 14:42:00 +0100

    creating get delete and patch on attendees controller

--- controllers/attendeesController.js
+++ controllers/attendeesController.js
@@ -47,3 +47,77 @@ export const createAttendee = async (req, res) => {
     await closeDatabase();
   }
 };
+
+// Get single attendee by ID
+// GET
+
+export const getSingleAttendeeById = async (req, res) => {
+  try {
+    const database = await connectDatabase();
+    const { id } = req.params;
+    const attendee = await database
+      .collection("attendees")
+      .findOne({ _id: new ObjectId(id) });
+    if (!attendee) {
+      return res.status(404).json({ error: "Attendee not found" });
+    }
+    res.status(200).json(attendee);
+  } catch (error) {
+    // Handle the error here
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  } finally {
+    await closeDatabase();
+  }
+};
+
+// Delete attendee
+// DELETE
+
+export const deleteAttendee = async (req, res) => {
+  try {
+    const database = await connectDatabase();
+    const { id } = req.params;
+    const attendee = await database
+      .collection("attendees")
+      .deleteOne({ _id: new ObjectId(id) });
+    if (attendee.deletedCount === 0) {
+      return res.status(404).json({ error: "Attendee not found" });
+    }
+    res.status(200).json({ message: "Attendee deleted" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  } finally {
+    await closeDatabase();
+  }
+};
+
+// Update attendee
+// PATCH
+export const updateAttendee = async (req, res) => {
+  try {
+    const database = await connectDatabase();
+    const { id } = req.params;
+    const updateData = req.body;
+
+    if (Object.keys(updateData).length === 0) {
+      return res.status(400).json({ error: "No update fields provided" });
+    }
+
+    const attendee = await database
+      .collection("attendees")
+      .updateOne({ _id: new ObjectId(id) }, { $set: updateData });
+
+    if (attendee.matchedCount === 0) {
+      return res.status(404).json({ error: "Attendee not found" });
+    }
+
+    res.status(200).json({ message: "Attendee updated" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  } finally {
+    await closeDatabase();
+  }
+};
--- routes/attendeesRoutes.js
+++ routes/attendeesRoutes.js
@@ -1,7 +1,15 @@
 import express from "express";
-import { createAttendee } from "../controllers/attendeesController.js";
+import {
+  createAttendee,
+  getSingleAttendeeById,
+  deleteAttendee,
+  updateAttendee,
+} from "../controllers/attendeesController.js";
 const router = express.Router();
 
 router.post("/", createAttendee);
+router.get("/:id", getSingleAttendeeById);
+router.delete("/:id", deleteAttendee);
+router.patch("/:id", updateAttendee);
 
 export default router;
